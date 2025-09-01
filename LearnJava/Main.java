04686f95b1c5340f6a6560c409145d2a99090efd
jonathan strid <pjstrid@outlook.com>
2025-09-01 09:44:27 +0200

    update

---
+++ LearnJava/Main.java
@@ -0,0 +1,15 @@
+package LearnJava;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte age = 31;
+        Date now = new Date();
+        System.out.println(now);
+
+    }
+    
+
+}
